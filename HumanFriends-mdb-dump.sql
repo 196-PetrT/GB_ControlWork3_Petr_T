494126c0f8377c8fe90f467268f74e1e9f2a50b7
PetrT <140140070+196-PetrT@users.noreply.github.com>
2024-12-19 19:12:06 +0300

    edit task6 and working to task7

---
+++ HumanFriends-mdb-dump.sql
@@ -0,0 +1,11 @@
+-- MariaDB dump 10.19  Distrib 10.6.20-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: HumanFriends
+-- ------------------------------------------------------
+-- Server version	10.6.20-MariaDB-ubu2204
+
+-- Dump completed on 2024-12-19 19:46:11
+
+create database if not exists HumanFriends;
+
+USE HumanFriends;
